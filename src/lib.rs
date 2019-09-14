037d55f8a76196b5385206113c8f074aa140339c
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2019-09-14 02:15:19 +0200

    Add the first batch of unit tests for the VM's instructions prior to implementing them.

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,7 @@ pub mod chip8 {
         pub fn new() -> Self {
             let mut memory = [0; MEMORY_SIZE];
             memory[..0x50].clone_from_slice(&SPRITES[..0x50]);
+
             Emulator {
                 program_counter: 0x200,
                 memory,
@@ -51,21 +52,6 @@ pub mod chip8 {
             }
         }
 
-        /// The instructions are stored big endian and are 16 bits large.
-        fn fetch_opcode(&self) -> u16 {
-            let first_byte: u8 = self.memory[self.program_counter as usize];
-            let second_byte: u8 = self.memory[(self.program_counter + 1) as usize];
-
-            ((first_byte as u16) << 8) | (second_byte as u16)
-        }
-
-        pub fn instruction_cycle(&mut self) {
-            // Fetch
-            let _opcode: u16 = self.fetch_opcode();
-            self.program_counter += 2;
-            // Decode & execute
-        }
-
         pub fn pixel_at(&self, x: usize, y: usize) -> bool {
             assert!(x < DISPLAY_WIDTH);
             assert!(y < DISPLAY_HEIGHT);
@@ -73,7 +59,28 @@ pub mod chip8 {
             self.display[DISPLAY_HEIGHT - y - 1][x]
         }
 
-        fn sys_addr(&mut self, nnn: u16) {}
+        /// The CHIP-8's fetch, decode, and execute instruction cycle.
+        pub fn instruction_cycle(&mut self) {
+            let opcode: u16 = self.fetch(self.program_counter as usize);
+            self.program_counter += 2;
+            self.decode_and_execute(opcode);
+        }
+
+        fn fetch(&self, program_counter: usize) -> u16 {
+            // The instructions are stored big endian and are 16 bits large
+            let first_byte: u8 = self.memory[program_counter];
+            let second_byte: u8 = self.memory[program_counter + 1];
+
+            ((first_byte as u16) << 8) | (second_byte as u16)
+        }
+
+        fn decode_and_execute(&mut self, opcode: u16) {
+            // TODO: Match opcode to instruction
+        }
+
+        fn sys_addr(&mut self, nnn: u16) {
+            unimplemented!("This instruction [0nnn - SYS addr] has not been implemented.");
+        }
 
         fn cls(&mut self) {}
 
@@ -147,11 +154,12 @@ pub mod chip8 {
     // Unit tests
     #[cfg(test)]
     mod tests {
-        use super::*;
+        use super::Emulator;
 
         #[test]
-        fn constructor() {
+        fn test_constructor() {
             let chip8 = Emulator::new();
+
             // Program counter
             assert_eq!(
                 chip8.program_counter, 0x200,
@@ -181,6 +189,148 @@ pub mod chip8 {
                     assert_eq!(chip8.pixel_at(x, y), false);
                 }
             }
+        }
+
+        #[test]
+        #[should_panic]
+        fn test_sys_addr() {
+            let mut chip8 = Emulator::new();
+
+            chip8.sys_addr(0);
+        }
+
+        #[test]
+        fn test_cls() {
+            let mut chip8 = Emulator::new();
+            for row in chip8.display.iter_mut() {
+                for pixel in row.iter_mut() {
+                    *pixel = true;
+                }
+            }
+
+            chip8.cls();
+
+            assert!(chip8
+                .display
+                .iter()
+                .all(|&row| row.iter().all(|&pixel| !pixel)));
+        }
+
+        #[test]
+        fn test_ret() {
+            let mut chip8 = Emulator::new();
+            // Push old program counter onto stack
+            let old_address: u16 = chip8.stack[chip8.stack_pointer as usize];
+            chip8.stack[chip8.stack_pointer as usize] = 1;
+            chip8.stack_pointer += 1;
+            // Set program counter to a different address
+            chip8.program_counter = 0xF03D;
+
+            chip8.ret();
+
+            assert_eq!(chip8.stack_pointer, 0);
+            assert_eq!(chip8.program_counter, old_address);
+        }
+
+        #[test]
+        fn test_jp_addr() {
+            let mut chip8 = Emulator::new();
+            let nnn: u16 = 0xFD0;
+
+            chip8.jp_addr(nnn);
+
+            assert_eq!(chip8.program_counter, nnn);
+        }
+
+        #[test]
+        fn test_call_addr() {
+            let mut chip8 = Emulator::new();
+            let nnn: u16 = 0x2E6;
+
+            chip8.call_addr(nnn);
+
+            assert_eq!(chip8.program_counter, nnn);
+            assert_eq!(chip8.stack_pointer, 1);
+            assert_eq!(chip8.stack[chip8.stack_pointer as usize], 0x200);
+        }
+
+        #[test]
+        fn test_se_vx_byte_skip() {
+            let mut chip8 = Emulator::new();
+            let x: usize = 0x0;
+            let kk: u8 = 0x00;
+
+            assert_eq!(chip8.v[x], kk);
+
+            chip8.se_vx_byte(x, kk);
+
+            assert_eq!(chip8.program_counter, 0x202);
+        }
+
+        #[test]
+        fn test_se_vx_byte_no_skip() {
+            let mut chip8 = Emulator::new();
+            let x: usize = 0x0;
+            let kk: u8 = 0x04;
+
+            assert_ne!(chip8.v[x], kk);
+
+            chip8.se_vx_byte(x, kk);
+
+            assert_eq!(chip8.program_counter, 0x200);
+        }
+
+        #[test]
+        fn test_sne_vx_byte_skip() {
+            let mut chip8 = Emulator::new();
+            let x: usize = 0x0;
+            let kk: u8 = 0x04;
+
+            assert_ne!(chip8.v[x], kk);
+
+            chip8.sne_vx_byte(x, kk);
+
+            assert_eq!(chip8.program_counter, 0x202);
+        }
+
+        #[test]
+        fn test_sne_vx_byte_no_skip() {
+            let mut chip8 = Emulator::new();
+            let x: usize = 0x0;
+            let kk: u8 = 0x00;
+
+            assert_eq!(chip8.v[x], kk);
+
+            chip8.sne_vx_byte(x, kk);
+
+            assert_eq!(chip8.program_counter, 0x200);
+        }
+
+        #[test]
+        fn test_se_vx_vy_skip() {
+            let mut chip8 = Emulator::new();
+            let x: usize = 0x0;
+            let y: usize = 0x1;
+
+            assert_eq!(chip8.v[x], chip8.v[y]);
+
+            chip8.se_vx_vy(x, y);
+
+            assert_eq!(chip8.program_counter, 0x202);
+        }
+
+        #[test]
+        fn test_se_vx_vy_no_skip() {
+            let mut chip8 = Emulator::new();
+            let x: usize = 0x0;
+            let y: usize = 0x1;
+            chip8.v[y] = 1;
+
+            assert_ne!(chip8.v[x], chip8.v[y]);
+
+            chip8.se_vx_vy(x, y);
+
+            assert_eq!(chip8.program_counter, 0x200);
         }
     }
 }
